a347ff2aa1a04f86d232b65f2549c2237fff8666
fahim-morshed-1102 <bsse1102@iit.du.ac.bd>
2022-03-29 18:35:16 +0600

    implemented deck and card

---
+++ src/Card.java
@@ -0,0 +1,68 @@
+public class Card implements Cardable{
+    private final int value;
+    private final Suit suit;
+    private boolean selected;
+    private boolean faceUp;
+
+    public Card(int value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+        this.selected = false;
+        this.faceUp = false;
+    }
+
+    @Override
+    public boolean getSelected() {
+        return selected;
+    }
+
+    @Override
+    public boolean getFaceUp() {
+        return faceUp;
+    }
+
+    @Override
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public void switchSelectedState() {
+        selected = !selected;
+    }
+
+    @Override
+    public void resetSelected() {
+        selected = false;
+    }
+
+    @Override
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+
+    @Override
+    public String toString(){
+        String returnString = "";
+
+        if(value<=10 && value>=2)
+            returnString+= String.valueOf(value);
+        else if(value == 11)
+            returnString+="J";
+        else if(value == 12)
+            returnString+="Q";
+        else if(value == 13)
+            returnString+="K";
+        else returnString+="A";
+
+        if(suit.equals(Suit.CLUB))
+            returnString+="♣";
+        else if(suit.equals(Suit.DIAMOND))
+            returnString+="♦";
+        else if(suit.equals(Suit.HEART))
+            returnString+="♥";
+        else returnString+="♠";
+
+        return returnString;
+    }
+}
---
+++ src/Deck.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Deck implements Deckable{
+    private static Deck instance; //singleton
+    private final List<Cardable> deck = new ArrayList<>();
+
+    private Deck() {
+        instance = this;
+        for (int i=0; i<4; i++){
+            for(int j=1;j<14;j++){
+                if(i==0) deck.add(new Card(j,Cardable.Suit.DIAMOND));
+                else if(i==1) deck.add(new Card(j, Cardable.Suit.HEART));
+                else if(i==2) deck.add(new Card(j, Cardable.Suit.CLUB));
+                else deck.add(new Card(j, Cardable.Suit.SPADE));
+            }
+        }
+    }
+
+    @Override
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+
+    @Override
+    public void returnToDeck(LinkedList<Cardable> discarded) {
+        deck.addAll(discarded);
+        shuffle();
+    }
+
+    @Override
+    public Cardable drawACard(boolean faceUp) {
+        Cardable card = deck.remove(0);
+        card.setFaceUp(faceUp);
+        return card;
+    }
+
+    public Deck getInstance(){
+        if(instance == null)
+            instance = new Deck();
+        return instance;
+    }
+}
